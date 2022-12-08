008ee4cc9e45686ce387191315e02c596b23a29c
Beth9528 <115501520+Beth9528@users.noreply.github.com>
2022-12-07 22:42:09 -0600

    Add files via upload

---
+++ SpringbootJdbcDemoApplicationTests.java
@@ -0,0 +1,41 @@
+package com.springboot.springboot;
+
+import java.util.List;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+
+//@RunWith(SpringJUnit5Runner.class)
+@SpringBootConfiguration
+	public class SpringbootJdbcDemoApplicationTests
+	{
+	    @Autowired
+	    private UserRepository userRepository;
+	 
+	    @Test
+	    public void findAllUsers() {
+	        List<User> users = userRepository.findAll();
+	        assertNotNull(users);
+	        assertTrue(!users.isEmpty());
+	    }
+	 
+	    @Test
+	    public void findUserById() {
+	        User user = userRepository.findUserById(1);
+	        assertNotNull(user);
+	    }
+	 
+	    @Test
+	    public void createUser() {
+	        User user = new User(0, "John", "john@gmail.com");
+	        User savedUser = userRepository.create(user);
+	        User newUser = userRepository.findUserById(savedUser.getId());
+	        assertNotNull(newUser);
+	        assertEquals("John", newUser.getName());
+	        assertEquals("john@gmail.com", newUser.getEmail());
+	    }
+	}
+
+}
